b369ad8de4cfba6ca84688641b7b4f89bdce2720
Ark2K <ankurravindrakushwaha@gmail.com>
2022-01-28 16:28:02 +0530

    add signin and signout buttons

--- src/App.js
+++ src/App.js
@@ -27,4 +27,20 @@ function App() {
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {}
+
 export default App;
